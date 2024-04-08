64042502979132209bedd00078effae8c6c22ac0
Gabriel <140923456+SrGabriel1@users.noreply.github.com>
2024-04-07 17:38:29 -0700

    Update Comida.java

--- PotrosPedidos_Negocio/src/main/java/Mock/Comida.java
+++ PotrosPedidos_Negocio/src/main/java/Mock/Comida.java
@@ -9,5 +9,49 @@ package Mock;
  * @author yohan
  */
 public class Comida {
+    String nombre;
+    String tipo;
+    String cantidad;
+    String disponible;
+
+    public Comida(String nombre, String tipo, String cantidad, String disponible) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.disponible = disponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(String disponible) {
+        this.disponible = disponible;
+    }
     
+            
 }
