e4111bdff0327f9aa0e248235ab12467eff397db
Gabriel <140923456+SrGabriel1@users.noreply.github.com>
2024-04-07 17:26:42 -0700

    creado el paquete de negocio

---
+++ PotrosPedidos_Negocio/src/main/java/com/mycompany/potrospedidos_negocio/PotrosPedidos_Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.potrospedidos_negocio;
+
+/**
+ *
+ * @author yohan
+ */
+public class PotrosPedidos_Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
