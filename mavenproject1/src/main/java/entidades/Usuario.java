8b161f3b1719551358ea400d7eef535dd2981bff
Madero26 <jose.madero244903@potros.itson.edu.mx>
2024-04-14 17:33:35 -0700

    Cree persistencia
    ando mapeando apenas

---
+++ mavenproject1/src/main/java/entidades/Usuario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jl4ma
+ */
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+
+    
+}
