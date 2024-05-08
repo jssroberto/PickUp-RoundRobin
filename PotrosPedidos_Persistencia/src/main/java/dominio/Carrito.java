6e3ffbde20f7d1bc447eff0d817d962a54d42ea5
Madero26 <jose.madero244903@potros.itson.edu.mx>
2024-05-07 20:33:19 -0700

    nuevas entidades
    producto, carrito y detalle carrito

--- PotrosPedidos_Persistencia/src/main/java/dominio/Carrito.java
+++ PotrosPedidos_Persistencia/src/main/java/dominio/Carrito.java
@@ -4,10 +4,61 @@
  */
 package dominio;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.types.ObjectId;
+
 /**
  *
  * @author jl4ma
  */
 public class Carrito {
+    
+    private ObjectId id;
+    private Float total;
+    private List<DetalleProducto> productos;
+
+    public Carrito(ObjectId id, Float total, List<DetalleProducto> productos) {
+        this.id = id;
+        this.total = total;
+        this.productos = productos;
+        productos = new ArrayList<>();
+    }
+
+    public Carrito(Float total, List<DetalleProducto> productos) {
+        this.total = total;
+        this.productos = productos;
+        productos = new ArrayList<>();
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public List<DetalleProducto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<DetalleProducto> productos) {
+        this.productos = productos;
+    }
+
+    @Override
+    public String toString() {
+        return "Carrito{" + "id=" + id + ", total=" + total + ", productos=" + productos + '}';
+    }
+    
     
 }
--- PotrosPedidos_Persistencia/src/main/java/dominio/Producto.java
+++ PotrosPedidos_Persistencia/src/main/java/dominio/Producto.java
@@ -4,10 +4,85 @@
  */
 package dominio;
 
+import org.bson.types.ObjectId;
+
 /**
  *
  * @author jl4ma
  */
 public class Producto {
+    
+    
+    private ObjectId id;
+    private String nombre;
+    private Float precio;
+    private String descripcion;
+    private String direccionImagen;
+
+    public Producto(ObjectId id, String nombre, Float precio, String descripcion, String direccionImagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.direccionImagen = direccionImagen;
+    }
+
+    public Producto(ObjectId id) {
+        this.id = id;
+    }
+
+    public Producto(String nombre, Float precio, String descripcion, String direccionImagen) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.direccionImagen = direccionImagen;
+    }
+
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Float precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDireccionImagen() {
+        return direccionImagen;
+    }
+
+    public void setDireccionImagen(String direccionImagen) {
+        this.direccionImagen = direccionImagen;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", descripcion=" + descripcion + ", direccionImagen=" + direccionImagen + '}';
+    }
+    
     
 }
